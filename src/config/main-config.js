d919b2b6f4db864506514db36fda46705fdb7f59
sqwisha <40513088+sqwisha@users.noreply.github.com>
2019-04-21 12:37:12 -0700

    checkpoint/routing controllers and the view done

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -1,5 +1,11 @@
-module.exports = {
-  init() {
+require('dotenv').config();
+const path = require('path');
+const viewsFolder = path.join(__dirname, '..', 'views');
 
+module.exports = {
+  init(app, express) {
+    app.set('views', viewsFolder);
+    app.set('view engine', 'ejs');
+    app.use(express.static(path.join(__dirname, '..', 'assets')));
   }
 };
