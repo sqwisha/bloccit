9ea973c65327a8468762cb494a274314f0141ada
sqwisha <40513088+sqwisha@users.noreply.github.com>
2019-04-28 15:35:46 -0700

    checkpoint/post resource 1 done

--- src/db/models/topic.js
+++ src/db/models/topic.js
@@ -1,8 +1,14 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   var Topic = sequelize.define('Topic', {
-    title: DataTypes.STRING,
-    description: DataTypes.STRING
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   }, {});
   Topic.associate = function(models) {
     Topic.hasMany(models.Banner, {
@@ -12,6 +18,10 @@ module.exports = (sequelize, DataTypes) => {
     Topic.hasMany(models.Rule, {
       foreignKey: 'ruleId',
       as: 'rules',
+    });
+    Topic.hasMany(models.Post, {
+      foreignKey: 'topicId',
+      as: 'posts'
     });
   };
   return Topic;
