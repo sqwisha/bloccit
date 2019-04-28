db4093f2632631a6446151fceb81c44da73d48f2
sqwisha <40513088+sqwisha@users.noreply.github.com>
2019-04-28 12:33:05 -0700

    assignment/topic resource 2 done

---
+++ src/controllers/adController.js
@@ -0,0 +1,65 @@
+const adQueries = require('../db/queries.ads.js');
+
+module.exports = {
+  index(req, res, next) {
+    adQueries.getAllAds((err, ads) => {
+      if (err) {
+        res.redirect(500, 'static/index');
+      } else {
+        res.render('ads/index', {ads});
+      }
+    });
+  },
+  new(req, res, next){
+    res.render('ads/new');
+  },
+  create(req, res, next) {
+    let newAd = {
+      title: req.body.title,
+      description: req.body.description
+    };
+    adQueries.addAd(newAd, (err, ad) => {
+      if (err) {
+        res.redirect(500, '/ads/new');
+      } else {
+        res.redirect(303, `/ads/${ad.id}`);
+      }
+    });
+  },
+  show(req, res, next) {
+    adQueries.getAd(req.params.id, (err, ad) => {
+      if (err || ad === null) {
+        res.redirect(404, '/');
+      } else {
+        res.render('ads/show', {ad});
+      }
+    });
+  },
+  destroy(req, res, next) {
+    adQueries.deleteAd(req.params.id, (err, ad) => {
+      if (err) {
+        res.redirect(500, `/ads/${ad.id}`);
+      } else {
+        res.redirect(303, '/ads');
+      }
+    });
+  },
+  edit(req, res, next) {
+    adQueries.getAd(req.params.id, (err, ad) => {
+      if (err) {
+        res.redirect(404, '/');
+      } else {
+        res.render('ads/edit', {ad});
+      }
+    });
+  },
+  update(req, res, next) {
+    adQueries.updateAd(req.params.id, req.body, (err, ad) => {
+      if (err || ad === null) {
+        res.redirect(404, `/ads/${req.params.id}/edit`);
+      } else {
+        res.redirect(303, `/ads/${ad.id}`);
+      }
+    });
+  }
+};
