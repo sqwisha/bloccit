07926b6c60b7c17aa80182a784af93a51680de2d
sqwisha <40513088+sqwisha@users.noreply.github.com>
2019-05-05 21:31:30 -0700

    assignment/validations done

--- src/routes/validation.js
+++ src/routes/validation.js
@@ -9,10 +9,25 @@ module.exports = {
     const errors = req.validationErrors();
 
     if (errors) {
-      req.flash('errors', errors);
-      return res.redirect(303, req.headers.referrer);
+      req.flash('error', errors);
+      return res.redirect(303, req.headers.referer);
+    } else {
+      return next();
+    }
+  },
+  validateTopics(req, res, next) {
+    if(req.method === 'POST') {
+      req.checkBody('title', 'must be at least 5 characters in length').isLength({min: 5});
+      req.checkBody('description', 'must be at least 10 characters in length').isLength({min: 10});
+    }
+
+    const errors = req.validationErrors();
+
+    if (errors) {
+      req.flash('error', errors);
+      return res.redirect(303, req.headers.referer);
     } else {
       return next();
     }
   }
-};
+}
