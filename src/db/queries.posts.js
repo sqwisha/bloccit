3590046b2c0ee23d89af416de5144837cda9b7eb
sqwisha <40513088+sqwisha@users.noreply.github.com>
2019-05-12 15:04:02 -0700

    checkpoint/voting done

--- src/db/queries.posts.js
+++ src/db/queries.posts.js
@@ -3,6 +3,7 @@ const Topic = require('./models').Topic;
 const Authorizer = require('../policies/post');
 const Comment = require('./models').Comment;
 const User = require('./models').User;
+const Vote = require('./models').Vote;
 
 module.exports = {
   addPost(newPost, callback) {
@@ -19,7 +20,7 @@ module.exports = {
       include: [
         {model: Comment, as: 'comments', include: [
           {model: User}
-        ]}
+        ]}, {model: Vote, as: 'votes'}
       ]
     })
     .then((post) => {
